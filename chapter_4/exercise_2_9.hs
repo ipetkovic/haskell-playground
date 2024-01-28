664a4a6588f436071062126a5b3f39516c118325
Ivo Petkovic <ivo.petkovic@gmail.com>
2024-01-28 21:23:20 +0000

    update 4/2.9

--- chapter_4/exercise_2_9.hs
+++ chapter_4/exercise_2_9.hs
@@ -1,11 +1,16 @@
 myGroupBy :: (a -> a -> Bool) -> [a] -> [[a]]
-myGroupBy p xs = 
+myGroupBy p xs =
   let ys = drop 1 xs
       ps = zip xs ys
       z = case xs of
         [] -> []
         xs -> [[last xs]]
-  in foldr step z ps
+  in foldr mergeStep [] (foldr step z ps)
     where step (x, y) ((ch : ct) : at) | p x y = (x : ch : ct) : at
           step (x, y) (ah : at) = [x] : ah : at
-          step _ [] = []
+          step (x, y) [] = []
+
+          -- mergeStep :: [a] -> [[a]] -> [[a]]
+          mergeStep (x : xs) ((fh : ft) : at) | p x fh = ((x : xs) ++ (fh : ft )): at
+          mergeStep xs (ah : at)  = xs : ah : at
+          mergeStep xs [] = [xs]
